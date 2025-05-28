07139fe7a781a57a2e6c2f60c3fdae8baa5f06b4
Will Jacob <will.jacob99@outlook.com>
2025-05-28 09:09:59 +0100

    added operational hours

--- pir.py
+++ pir.py
@@ -6,8 +6,7 @@ import datetime
 SLEEP_TIME = 30
 DEBOUNCE_TIME = 10
 
-def shutdown():
-    ciscoDevice = {
+ciscoDevice = {
         'device_type': 'cisco_ios',
         'ip': '10.1.10.24',
         'username': 'optiadmin',
@@ -16,6 +15,11 @@ def shutdown():
         'secret': 'optiadmin',
     }
 
+pir = MotionSensor(4)
+lastMotionTime = None
+portIsUp = False
+
+def shutdown(ciscoDevice):
     connection = ConnectHandler(**ciscoDevice)
     connection.enable()
 
@@ -24,16 +28,7 @@ def shutdown():
 
     connection.disconnect()
 
-def noShutdown():
-    ciscoDevice = {
-        'device_type': 'cisco_ios',
-        'ip': '10.1.10.24',
-        'username': 'optiadmin',
-        'password': 'optiadmin',
-        'port': 22,
-        'secret': 'optiadmin',
-    }
-
+def noShutdown(ciscoDevice):
     connection = ConnectHandler(**ciscoDevice)
     connection.enable()
 
@@ -42,34 +37,51 @@ def noShutdown():
 
     connection.disconnect()
 
-pir = MotionSensor(4)
-lastMotionTime = None
-portIsUp = False
+def handleMotion():
+    global lastMotionTime, portIsUp
+    nowTime = time.time()
 
-while True:
-    if pir.motion_detected:
-        nowTime = time.time()
+    if lastMotionTime is None or (nowTime - lastMotionTime) > DEBOUNCE_TIME:
+        now = datetime.datetime.now()
+        print("[MOTION DETECTED]:", now.strftime("%Y-%m-%d %H:%M:%S"))
+        lastMotionTime = nowTime
 
-        if lastMotionTime is None or (nowTime - lastMotionTime) > DEBOUNCE_TIME:
+    if not portIsUp:
+        noShutdown(ciscoDevice)
+        portIsUp = True
+
+def handleNoMotion():
+    global lastMotionTime, portIsUp
+
+    if portIsUp and lastMotionTime is not None:
+        if (time.time() - lastMotionTime) > SLEEP_TIME:
             now = datetime.datetime.now()
-            print("[MOTION DETECTED]:", now.strftime("%Y-%m-%d %H:%M:%S"))
-            lastMotionTime = nowTime
+            print("[SHUTTING] no activity detected for 30 seconds:", now.strftime("%Y-%m-%d %H:%M:%S"))
+            shutdown(ciscoDevice)
+            portIsUp = False
+            lastMotionTime = None        
 
-        if not portIsUp:
-            noShutdown()
-            portIsUp = True
+def motionLoop():
+    while True:
+        if pir.motion_detected:
+            handleMotion()
+        else:
+            handleNoMotion()
+        time.sleep(1)
+
+def monitorTimes():
+    now = datetime.datetime.now()
+    return now.hour < 6 or now.hour > 20
+    #return now.hour > 0
+    
+if __name__ == "__main__":
+    if monitorTimes():
+        motionLoop()
     else:
-        if portIsUp and lastMotionTime is not None:
-            if (time.time() - lastMotionTime) > SLEEP_TIME:
-                now = datetime.datetime.now()
-                print("[SHUTTING] no activity detected for 30 seconds:", now.strftime("%Y-%m-%d %H:%M:%S"))
-                shutdown()
-                portIsUp = False
-                lastMotionTime = None
-
-    time.sleep(1)
+        noShutdown(ciscoDevice)
 
 #log actual time of event
-#check initial state of wap
 #add operational hours
 #log events into csv file
+#calculate power saved and carbon footprint
+#add more ports
